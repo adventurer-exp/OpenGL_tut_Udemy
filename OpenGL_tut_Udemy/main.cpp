3d749c56e99c2b42ebebddd7ae8f13f65ad2a8fb
SimonaBilkova(s3748189) <s3748189@student.rmit.edu.au>
2021-01-11 22:45:20 +1100

    Initial Commit

---
+++ OpenGL_tut_Udemy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_tut_Udemy
+//
+//  Created by Simona Bilkova on 11/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
